56d0f0bd1fc1a5fd3f15d93548284a5a0a0ab1ca
Tatyana Abramova <tanusha-atv@mail.ru>
2022-06-27 12:29:54 +0500

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number < 1000)
+{
+    Console.WriteLine(number / 10 % 10);
+}
+else
+{
+    Console.WriteLine("Число введено некорретно! Введите ТРЕХЗНАЧНОЕ число!");
+}
